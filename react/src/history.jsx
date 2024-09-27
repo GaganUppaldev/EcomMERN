f988d6ff4304badeca332f2acf42cf01877dbdd2
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-27 21:40:15 +0530

    Create history.jsx
    created history.jsx and added function

---
+++ react/src/history.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function History(){
+    return(
+        <>
+
+        <div id="history1">
+
+        </div>
+        
+
+
+        
+        </>
+        
+    )
+    
+}
+
+export default History;
