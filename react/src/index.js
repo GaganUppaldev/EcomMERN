58a878ddbfbd93a1b36d964022e82edc16141bf1
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-27 21:31:55 +0530

    Update index.js
    according to all new new .jsx files and functions inside them created

--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from './Nav.jsx';
+import Box2 from './box2.jsx';
+import Arival from './arrival.jsx';
+import Product from './products.jsx';
+import Deal from './deal.jsx';
+import History from './history.jsx';
+import Pro from './pro.jsx';
+import Footer from './footer.jsx';
 import './index.css';
 import './images/cart.svg';
 
@@ -9,4 +16,53 @@ test.render(
     <React.StrictMode>
     <Main />
     </React.StrictMode>
+) 
+
+const Box2data = ReactDOM.createRoot(document.getElementById("box2"));
+Box2data.render(
+  <React.StrictMode>
+  <Box2 />
+  </React.StrictMode>
+)
+
+const arival = ReactDOM.createRoot(document.getElementById("arrivals"));
+arival.render(
+  <React.StrictMode>
+    <Arival />
+  </React.StrictMode>
+)
+
+const good = ReactDOM.createRoot(document.getElementById("list"));
+good.render(
+  <React.StrictMode>
+    <Product />
+  </React.StrictMode>
+)
+
+const offer = ReactDOM.createRoot(document.getElementById("deal"));
+offer.render(
+  <React.StrictMode>
+    <Deal />
+  </React.StrictMode>
+)
+
+const his = ReactDOM.createRoot(document.getElementById("history"));
+his.render(
+  <React.StrictMode>
+    <History />
+  </React.StrictMode>
+)
+
+const benifits = ReactDOM.createRoot(document.getElementById("pros"));
+benifits.render(
+  <React.StrictMode>
+    <Pro />
+  </React.StrictMode>
+)
+
+const foot = ReactDOM.createRoot(document.getElementById("footer"));
+foot.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>
 )
