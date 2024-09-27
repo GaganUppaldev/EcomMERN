14d0840e517a9a1177a9b7bc91a2d69aba1c96d0
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-27 21:35:19 +0530

    Create box2.jsx
    created box2.jsx

---
+++ react/src/box2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function Box2(){
+    return(
+        <>
+        <div id="heropic">
+
+        <div id="box2A"></div>
+        <div id="box2B"></div>
+
+
+        </div>
+        </>
+        
+    )
+    
+}
+
+export default Box2;
