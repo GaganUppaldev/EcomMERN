a10155b9ff762eb140011d4eab1af2bffd991c68
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-27 21:36:30 +0530

    Create deal.jsx
    created deal.jsx

---
+++ react/src/deal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Deal(){
+    return(
+        <>
+        <div id="special-deal">
+
+        </div>
+        </>
+        
+    )
+    
+}
+
+export default Deal;
