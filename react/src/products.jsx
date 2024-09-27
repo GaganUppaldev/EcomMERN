73ed88ef10f627c0ffc1b00a0d53abddf93b9edd
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-27 21:43:14 +0530

    Create products.jsx
    created products.jsx and function inside it then exported the divs that function exported

---
+++ react/src/products.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Product() {
+    return (
+        <>    
+            <div id="intro"></div>
+
+            <div id="prod">
+                <div className="prod"></div>
+                <div className="prod"></div>
+                <div className="prod"></div>
+                <div className="prod"></div>
+                <div className="prod"></div>
+            </div>
+
+            <div id="prod2">
+                <div className="prod2"></div>
+                <div className="prod2"></div>
+                <div className="prod2"></div>
+                <div className="prod2"></div>
+                <div className="prod2"></div>
+            </div>
+        </>
+    );
+}
+
+export default Product;
+
