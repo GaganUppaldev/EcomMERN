6d351c3bdedede0c5a3f33d5de285f905aa165d5
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-28 21:34:42 +0530

    Update history.jsx
    modified

--- react/src/history.jsx
+++ react/src/history.jsx
@@ -3,10 +3,16 @@ function History(){
     return(
         <>
 
-        <div id="history1">
+<div id="history1">
+    <h1>Our History</h1>
+    <p>
+        One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.
+    </p>
+    <p>
+        He must have tried it a hundred times, shut his eyes so that he wouldn't have to look at the floundering legs, and only stopped when he began to feel a mild, dull pain there that he had never felt before.
+    </p>
+</div>
 
-        </div>
-        
 
 
         
