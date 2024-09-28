ffb54be56b5df5ec1a5d4b971398021f13bbb97b
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-28 21:32:10 +0530

    Update arrival.jsx
    updated

--- react/src/arrival.jsx
+++ react/src/arrival.jsx
@@ -2,10 +2,26 @@ import React from "react";
 function Arival(){
     return(
         <>
-        <div id="update">
-            
-        </div>
-        </>
+
+<div id="update">
+    <div id="update-A">
+
+    <h2 id="update-heading">New Arrivals</h2>
+    <div class="arrival-section">
+        <h3>Gregor Samsa's Transformation</h3>
+        <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+    </div>
+    <div class="arrival-section">
+        <h3>The Strange Appearance</h3>
+        <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>
+    </div>
+                                                      
+                                                
+    </div> 
+
+   </div>
+
+    </>
     )
 }
 export default  Arival;
