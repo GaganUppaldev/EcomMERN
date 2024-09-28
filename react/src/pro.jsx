e8af122abb624772c55f4f57734fe464cd2c1281
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-28 21:36:29 +0530

    Update pro.jsx
    modified

--- react/src/pro.jsx
+++ react/src/pro.jsx
@@ -2,9 +2,57 @@ import React from "react";
 function Pro(){
     return(
         <>
-        <div id="pro1"></div>
-        </>
+<div id="pro1">
+    <div class="feature">
         
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+            <path d="M0 1a1 1 0 0 1 1-1h11.5a.5.5 0 0 1 .492.41l.5 3A.5.5 0 0 1 13 4H2.5L2 6H1a1 1 0 0 1-1-1V1zm2-1v2h10V0H2zm1 7v1h9V6H3v1zm-1 1v1h1v-1H2zm11 1v-1H4v1h9z"/>
+            <path fill-rule="evenodd" d="M14 3v2h-1V3h1zM1 7v2h11v1a3 3 0 1 1-6 0H6a3 3 0 1 1-6 0H0V7h1zm8 4a2 2 0 1 0 4 0 2 2 0 0 0-4 0zm5-1a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+        </svg>
+        <div class="feature-text">
+            <h4>Free Shipping & Return</h4>
+            <p>Free Shipping over $300</p>
+        </div>
+    </div>
+
+    <div class="feature">
+       
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M11 1a5 5 0 0 1 3.544 8.712c.682 1.335.701 2.444.039 2.86-.94.596-3.409.428-5.823-.768a13.4 13.4 0 0 1-4.4-3.432C3.042 7.944 3 7.223 3 7.223L1.856 6.159 3.414 4.6a5 5 0 0 1 7.586 0L11 4.465V1zm1 2v4h-2V3h2zm-2 5h2v1h-2V8zm.854 3.354a.5.5 0 0 0 .708-.708L11.5 9.707l-1.354 1.354a.5.5 0 0 0 .708.708z"/>
+            <path d="M3.29 11.72c.375-.432 1.118-1.3 2.095-1.986 1.426-.997 2.95-1.513 3.52-1.813l.387-.208-.432-.259a5.968 5.968 0 0 1-1.44-1.028c-.337-.311-.84-.647-1.494-.647a2 2 0 0 0-2 2c0 .552.195 1.07.518 1.481C3.63 9.983 2.99 11.183 3.29 11.72z"/>
+        </svg>
+        <div class="feature-text">
+            <h4>Money Back Guarantee</h4>
+            <p>30 Days Money Back Guarantee</p>
+        </div>
+    </div>
+
+    <div class="feature">
+       
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+            <path d="M3 1v1H2V1h1zM1 2h1v1H1V2zm1-1h1v1H2V1zm11 0a1 1 0 0 1 1 1v3.586a1 1 0 0 1-.293.707L7.707 13.293a1 1 0 0 1-1.414 0l-6-6A1 1 0 0 1 .293 7.293l6 6a1 1 0 0 0 1.414 0l5.586-5.586A1 1 0 0 0 14 4.586V1a1 1 0 0 1 1-1z"/>
+            <path d="M5.5 3a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+        </svg>
+        <div class="feature-text">
+            <h4>Best Prices</h4>
+            <p>Always the best prices</p>
+        </div>
+    </div>
+
+    <div class="feature">
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16">
+            <path d="M8 2a6 6 0 0 0-6 6v3.5a3.5 3.5 0 0 0 7 0V10a1 1 0 1 1 2 0v1.5a3.5 3.5 0 0 0 7 0V8a6 6 0 0 0-6-6H8z"/>
+            <path d="M4.5 8v2a.5.5 0 0 1-1 0V8a.5.5 0 0 1 1 0zm7 0v2a.5.5 0 0 1-1 0V8a.5.5 0 0 1 1 0z"/>
+        </svg>
+        <div class="feature-text">
+            <h4>24/7 Support</h4>
+            <p>020-800-456-747</p>
+        </div>
+    </div>
+</div>
+
+</>
     )
     
 }
